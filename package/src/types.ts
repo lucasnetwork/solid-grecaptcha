dc874230b7031a753572deaefdffdea71fc832df
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-09-13 14:40:25 -0300

    fix: remove types useless

--- package/src/types.ts
+++ package/src/types.ts
@@ -13,24 +13,7 @@ export interface ConfigRender {
      * Size of the rendered checkbox.
      * Defaults to "normal".
      */
-  size?: 'normal' | 'compact' | 'invisible'
-
-  /**
-     * Set a custom container for the challenge iframe by id or reference.
-     */
-  'challenge-container'?: HTMLElement | string
-
-  /**
-     * Language code as defined by ISO 639-1.
-     * Defaults to the browser local language.
-     * @see https://www.w3schools.com/tags/ref_language_codes.asp
-     */
-  hl?: string
-
-  /**
-     * See enterprise docs.
-     */
-  tplinks?: 'on' | 'off'
+  size?: 'normal' | 'compact'
 
   /**
      * Set the tabindex of the checkbox and challenge. When appropriate, this can make navigation of your site more intuitive.
@@ -38,29 +21,9 @@ export interface ConfigRender {
   tabindex?: number
 
   /**
-     * See enterprise docs.
-     */
-  custom?: boolean
-
-  /**
      * Callback function on successful captcha response, response token is passed back in function.
      */
   callback?: ((response: string) => void)
-
-  /**
-     * Callback function when greCaptcha challenge is displayed.
-     */
-  'open-callback'?: (() => void)
-
-  /**
-     * Callback function when greCaptcha challenge is hidden.
-     */
-  'close-callback'?: (() => void)
-
-  /**
-     * Callback function when greCaptcha challenge has expired.
-     */
-  'chalexpired-callback'?: (() => void)
 
   /**
      * Callback function when user's successful response has expired.
